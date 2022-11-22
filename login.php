7a32163bd9fb90af6e3e06b5848263dfc818d96f
Georgui716371 <azelps071del@gmail.com>
2022-11-22 08:56:01 -0500

    first commit

---
+++ login.php
@@ -0,0 +1,151 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="shortcut icon" href="" type="image/x-icon">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="CSS/style-login.css" type="text/css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <title>LOGIN</title>
+</head>
+
+<body>
+
+  <?php 
+   
+   include("PHP/header.php");
+  
+  ?>
+
+  <div class="container-fluid">
+    <br>
+    <br>
+  </div>
+
+  <div class="container-fluid">
+    <main class="form-signin w-100 m-auto p-5">
+      <form role="form" name="login" action="PHP/login.php" method="post">
+        <img class="mb-4" src="img/logo-pizza.png" alt="" width="70%" height="70%">
+        <h1 class="h3 mb-3 fw-normal">INICIAR SESIÓN </h1>
+
+        <div class="form-floating">
+		    <input type="text" class="form-control" id="username" name="username" placeholder="Nombre de usuario">
+        <label for="username">Nombre de usuario o email</label>  
+      </div>
+        <div class="form-floating">
+		    <input type="password" class="form-control" id="password" name="password" placeholder="Contrase&ntilde;a">
+        <label for="password">Contrase&ntilde;a</label>  
+      </div>
+        <button class="w-100 btn btn-lg btn-warning m-3" type="submit">INICIAR SESION</button>
+        <a class="w-100 btn btn-lg btn-warning m-3" href="registro.php">REGISTRARSE</a>
+
+      </form>
+    </main>
+    
+    <script>
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (function () {
+            'use strict'
+
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.querySelectorAll('.needs-validation')
+
+            // Loop over them and prevent submission
+            Array.prototype.slice.call(forms)
+                .forEach(function (form) {
+                    form.addEventListener('submit', function (event) {
+                        if (!form.checkValidity()) {
+                            event.preventDefault()
+                            event.stopPropagation()
+                        }
+
+                        form.classList.add('was-validated')
+                    }, false)
+                })
+        })()
+
+    </script>
+  </div>
+
+  <div class="container-fluid">
+    <footer class="py-5 p-4 mt-5">
+      <div class="row">
+        <div class="col-6 col-md-2 mb-3">
+          <h5>PIZZA AL PASO</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2">
+              <a href="" class="nav-link p-0 text-muted">NOSOTROS</a>
+            </li>
+            <li class="nav-item mb-2">
+              <a href="#" class="nav-link p-0 text-muted">HISTORIA</a>
+            </li>
+            <li class="nav-item mb-2">
+              <a href="#" class="nav-link p-0 text-muted">CONTACTANOS</a>
+            </li>
+            <li class="nav-item mb-2">
+              <a href="#" class="nav-link p-0 text-muted">PRODUCTOS</a>
+            </li>
+            <li class="nav-item mb-2">
+              <a href="#" class="nav-link p-0 text-muted">COMPRAR</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-6 col-md-2 mb-3">
+          <h5>Section</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2">
+              <a href="librodereclamaciones.php" class="nav-link p-0 text-muted">LIBRO DE RECLAMACIONES</a>
+            </li>
+            <li class="nav-item mb-2">
+              <a href="#" class="nav-link p-0 text-muted">OFERTAS</a>
+            </li>
+            <li class="nav-item mb-2">
+              <a href="#" class="nav-link p-0 text-muted">NOVEDADES</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="col-md-5 offset-md-1 mb-3">
+          <form>
+            <h5>Envianos un correo</h5>
+            <p>Monthly digest of what's new and exciting from us.</p>
+            <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+              <label for="newsletter1" class="visually-hidden">Correo Electronico</label>
+              <input id="newsletter1" type="text" class="form-control" placeholder="Email address" />
+              <button class="w-100 btn btn-lg btn-warning" type="button">Enviar</button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+        <p>&copy; 2022, Inc.Todos los derechos reservados.</p>
+        <ul class="list-unstyled d-flex">
+          <li class="ms-3">
+            <a class="link-dark" href="#"><svg class="bi" width="24" height="24">
+                <use xlink:href="#twitter" />
+              </svg></a>
+          </li>
+          <li class="ms-3">
+            <a class="link-dark" href="#"><svg class="bi" width="24" height="24">
+                <use xlink:href="#instagram" />
+              </svg></a>
+          </li>
+          <li class="ms-3">
+            <a class="link-dark" href="#"><svg class="bi" width="24" height="24">
+                <use xlink:href="#facebook" />
+              </svg></a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  </div>
+
+</body>
+
+</html>
